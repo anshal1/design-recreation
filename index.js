5c11a4958f732d456556199c29560da408644ee1
amshal1 <panshal911@gmail.com>
2024-06-25 17:46:43 +0530

    capsule image position bug partially fixed

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const capsule = document.querySelector(".capsule-section");
 const heroSection = document.querySelector(".hero-section");
 
-window.addEventListener("resize", () => {
+function setPosition() {
   const position = capsule.getBoundingClientRect();
   const parent_container_size = heroSection.getBoundingClientRect();
-  if (!width || !height) return;
   const x_position = Math.abs(parent_container_size.width - position.x);
   const y_position = Math.abs(parent_container_size.height - position.y);
+  heroSection.style.backgroundSize = `${parent_container_size.width}px ${parent_container_size.height}px`;
   capsule.style.backgroundPosition = `${x_position}px ${y_position}px`;
-});
+}
+
+window.addEventListener("resize", setPosition);
+
+setPosition();
